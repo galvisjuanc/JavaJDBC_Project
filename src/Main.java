d5a57dfac5a2ba05712ee4bc8dd1a227cdbd4f58
Juan Galvis <juan.galvis>
2024-03-04 07:55:18 -0500

    First Commit JDBC

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Connection myConnection = null;
+        Statement myStatement = null;
+        ResultSet myResultSet = null;
+    }
+}
