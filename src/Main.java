076794315ffa9864b13dddf9cd90b79dd02b6a46
Juan Galvis <juan.galvis>
2024-03-04 08:22:44 -0500

    Test connection and Add Statements

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
@@ -9,5 +7,21 @@ public class Main {
         Connection myConnection = null;
         Statement myStatement = null;
         ResultSet myResultSet = null;
+
+        try {
+            myConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/project", "root", "BrokenYouth_1711*");
+            System.out.println("Conexion Efectiva");
+
+            myStatement = myConnection.createStatement();
+            myResultSet = myStatement.executeQuery("SELECT * FROM employees");
+
+            while(myResultSet.next()) {
+                System.out.println(myResultSet.getString("first_name"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Conexion falló");
+
+        }
     }
 }
